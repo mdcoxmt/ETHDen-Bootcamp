4ba97ea87b3edb434583b4b4351dc54a19bac8a5
Duncan Cox <mdcoxmt@gmail.com>
2022-10-24 11:26:32 -0600

    Update Homework4.sol

--- Homework4.sol
+++ Homework4.sol
@@ -5,17 +5,27 @@ contract VolcanoCoin {
 
     uint256 supply = 10000;
     address owner;
-    event changeSupply(uint256);
-    address[] hodlers;
+    event changeSupply(uint256 indexed);
+    event transferbb(address, uint256, address);
+    mapping(address=>uint256) public balances;
+    
 
     constructor() {
-      owner = msg.sender;   
+      owner = msg.sender;
+      balances[owner] += supply;  
    }
 
    modifier onlyOwner {
       require(msg.sender == owner);
       _;
    }
+
+    struct payment {
+        uint256 payAmt;
+        address hodler;
+    }
+
+    payment[] pmtTracking;
 
     function whatSupply() public view returns (uint256){
         return supply;
@@ -27,5 +37,12 @@ contract VolcanoCoin {
         return supply; 
     }
 
+    function transfer(uint _amt, address _rcpt) public payable{
+        require(balances[msg.sender] <= _amt, "Get More Money");
+        balances[msg.sender] -= _amt;
+        balances[_rcpt] += _amt;
+        pmtTracking.push(payment({payAmt: _amt, hodler: _rcpt}));
+        emit transferbb(msg.sender, _amt, _rcpt);
+    }
 
 }
