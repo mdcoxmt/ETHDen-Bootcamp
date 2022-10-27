bbed46f510662a78745889d7207ba1647cd914dc
Duncan Cox <mdcoxmt@gmail.com>
2022-10-27 11:35:36 -0600

    Create Homework7.sol

---
+++ Homework7.sol
@@ -0,0 +1,56 @@
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity ^0.8.0;
+
+contract VolcanoCoin is Ownable {
+
+    uint256 supply = 10000;
+    /*address owner;*/
+    event changeSupply(uint256 indexed);
+    event transferbb(address, uint256, address);
+    mapping(address=>uint256) public balances;
+    
+
+    constructor() {
+      balances[owner()] = supply;  
+   }
+
+    struct payment {
+        address sender;
+        uint256 payAmt;
+        address hodler;
+    }
+
+    payment[] public transfers;
+    mapping(address => payment[]) public pmtTracking;
+
+    function whatSupply() public view returns (uint256){
+        return supply;
+    }
+
+    function increaseSupply() public onlyOwner returns (uint256){
+        supply += 1000;
+        emit changeSupply(supply);
+        return supply; 
+    }
+
+    function transfer(uint256 _amt, address _rcpt) public {
+        require(balances[msg.sender] >= _amt, "Get More Money");
+        balances[msg.sender] -= _amt;
+        balances[_rcpt] += _amt;
+        transfers.push(payment({sender: msg.sender, payAmt: _amt, hodler: _rcpt}));
+        /*pmtTracking[msg.sender]=transfers;*/
+        pmtRecord(msg.sender, _amt, _rcpt);
+        emit transferbb(msg.sender, _amt, _rcpt);
+    }
+
+     function getPayments(address user) public view returns (payment[] memory){
+        return pmtTracking[user];
+    }
+
+    function pmtRecord(address _sender, uint256 _payAmt, address _hodler) public {
+        pmtTracking[msg.sender].sender = _sender;
+        pmtTracking[msg.sender].payAmt = _payAmt;
+        pmtTracking[msg.sender].hodler = _hodler;
+    }
+}
