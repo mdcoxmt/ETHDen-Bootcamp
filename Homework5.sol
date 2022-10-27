f3b221132c5b1f3fc7d952cf1fd3f63d859e1422
Duncan Cox <mdcoxmt@gmail.com>
2022-10-27 10:53:14 -0600

    Create Homework5.sol

---
+++ Homework5.sol
@@ -0,0 +1,57 @@
+/*Add OpenZeppelin Ownable to the VolcanoCoin contract*/
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity ^0.8.0;
+
+contract VolcanoCoin is Ownable {
+
+    uint256 supply = 10000;
+    /*address owner;*/
+    event changeSupply(uint256 indexed);
+    event transferbb(address, uint256, address);
+    mapping(address=>uint256) public balances;
+    
+
+    constructor() {
+      balances[owner()] = supply;  
+   }
+
+  /* modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+   }*/
+
+    struct payment {
+        uint256 payAmt;
+        address hodler;
+    }
+
+    payment[] transfers;
+    mapping(address => payment[]) public pmtTracking;
+
+    function whatSupply() public view returns (uint256){
+        return supply;
+    }
+
+    function increaseSupply() public onlyOwner returns (uint256){
+        supply += 1000;
+        emit changeSupply(supply);
+        return supply; 
+    }
+
+    function transfer(uint256 _amt, address _rcpt) public {
+        require(balances[msg.sender] >= _amt, "Get More Money");
+        balances[msg.sender] -= _amt;
+        balances[_rcpt] += _amt;
+        transfers.push(payment({payAmt: _amt, hodler: _rcpt}));
+        pmtTracking[msg.sender]=transfers; 
+        emit transferbb(msg.sender, _amt, _rcpt);
+    }
+
+    
+     function getPayments(address user) public view returns (payment[] memory){
+        return pmtTracking[user];
+    }
+
+}
